71cb5a7e3add47a4efde4dd83dcdee5a34a957f0
Dan Guest <dguest@cern.ch>
2015-08-05 20:01:31 -0700

    initial commit

---
+++ bootstrap/plotting.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo apt-get install -y python3
+sudo apt-get install -y python3-scipy
+sudo apt-get install -y python3-matplotlib
+sudo apt-get install -y libhdf5-serial-dev
+sudo apt-get install -y hdf5-tools
---
+++ bootstrap/setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+## clone some things
+# start with my bash environment
+if [[ -d setup ]] ; then
+    rm -r setup
+fi
+git clone https://github.com/dguest/setup.git
+cat <<EOF > ~vagrant/.bashrc
+SETUP=~/setup
+. \${SETUP}/work_env.sh
+. \${SETUP}/bash_tools_common.sh
+EOF
