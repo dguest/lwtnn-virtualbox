c40ff540b74ae24f1fdaafda0d651231971d202f
Dan Guest <dguest@cern.ch>
2015-08-30 15:25:38 -0700

    add eigen, remove roofit

--- bootstrap/boost.sh
+++ bootstrap/boost.sh
@@ -2,3 +2,5 @@
 set -eu
 
 sudo apt-get install -y libboost-dev
+# also install eigen, why not?
+sudo apt-get install -y libeigen3-dev
--- bootstrap/root.sh
+++ bootstrap/root.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get install -y root-system-bin
 sudo apt-get install -y libroot-math-physics-dev
-sudo apt-get install -y libroot-roofit-dev
 sudo apt-get install -y libroot-montecarlo-eg-dev
 sudo apt-get install -y libroot-graf2d-postscript-dev
 
